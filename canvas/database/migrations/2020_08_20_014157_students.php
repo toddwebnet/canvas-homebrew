a28620a29ee71f304f34eca617c9e18d523bb5f6
James Todd <jtodd@pioneeringevolution.com>
2020-08-19 22:21:07 -0500

    doing some work

---
+++ canvas/database/migrations/2020_08_20_014157_students.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Students extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('family_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('school');
+            $table->string('grade');
+            $table->timestamps();
+            $table->foreign('family_id')->references('id')->on('families');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
---
+++ canvas/database/migrations/2020_08_20_014252_courses.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Courses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id')->nullable();
+            $table->unsignedBigInteger('canvas_id')->unique();
+            $table->string('name');
+            $table->string('code');
+            $table->string('uuid');
+            $table->dateTimeTz('start_at');
+            $table->string('calendar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
